6d28e0b6228d0c606af4382e21d198babee45be9
Alex Betts <alex.betts@gmail.com>
2022-02-04 14:49:46 -0500

    bbg: Introduce `setFocusableOnMousedown()` API to `BrowserWindow`
    This function can be used to control whether a `BrowserWindow` is activated when
    the mouse is clicked on it.

--- shell/browser/native_window_mac.mm
+++ shell/browser/native_window_mac.mm
@@ -1165,6 +1165,10 @@ bool NativeWindowMac::IsFocusable() {
   return ![window_ disableKeyOrMainWindow];
 }
 
+void NativeWindowMac::SetFocusableOnMousedown(bool focusableOnMousedown) {
+  [window_ setDisableKeyOrMainWindowOnMousedown:!focusableOnMousedown];
+}
+
 void NativeWindowMac::AddBrowserView(NativeBrowserView* view) {
   [CATransaction begin];
   [CATransaction setDisableActions:YES];
